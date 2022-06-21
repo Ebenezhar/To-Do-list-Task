c5aa4a8cd208e98f6d0e1595d2f3743e1fde34fe
Ebenezhar Selvakumar <103163165+Ebenezhar@users.noreply.github.com>
2022-06-21 10:08:00 +0530

    class 21.06.2022 guide

--- src/App.js
+++ src/App.js
@@ -3,20 +3,35 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let [myList,setList] = useState([])
-  let addItem = () => {
-    let newList = [...myList,{name:"new value",len:`${myList.length + 1}`}];
-    setList(newList);
+  const [list,setList] = useState([]);
+  const [currentTask,setCurrentTask] = useState("");
+  let addTask = () => {
+    setList([...list,{id:list.length + 1, name: currentTask,isDone : false }]);  
   }
-  return (
+
+  let markDone = (id) => {
+    console.log(id);
+    let itemIndex = list.findIndex(obj => obj.id === id);
+    list[itemIndex].isDone = true;
+    // list[id-1].isDone = true;
+    setList([...list]); 
+  }
+  let deleteContent = (id) => {
+    let itemIndex = list.findIndex(obj => obj.id === id);
+    console.log(list[itemIndex]);
+    list[itemIndex] = "";
+    setList([...list]); 
+  }
+    return (
     <div id='contain'>
-      <button onClick= {addItem}>Add item</button>
+      <input id='inp' type="text" onChange={(e) => setCurrentTask(e.target.value)}/>
+      <button onClick={addTask}>Add Task</button>
       <ul>
-        {
-          myList.map((item)=>{
-           return <li>{item.len}</li>
-          })
-        }
+      {
+        list.map((item)=>{
+          return <li className={item.isDone ? "mark-done" : ""}>{item.name} - <button onClick={() => markDone(item.id)}>Done</button><button onClick={() => deleteContent(item.id)}>Delete</button></li>
+        })
+      }
       </ul>
     </div>
   );
